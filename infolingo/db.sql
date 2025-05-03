d26f858ae0008209b6f4664c8f9bdec9be5bc810
Yevhenii <yevhenii.cherevko@edu.vs.ch>
2025-05-03 23:11:21 +0200

    La corréction du script

--- infolingo/db.sql
+++ infolingo/db.sql
@@ -7,19 +7,24 @@ profile_photo TEXT
 
 CREATE TABLE IF NOT EXISTS questions(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-FOREIGN KEY (user_id) REFERENCES users(id),
-content TEXT NOT NULL
-mark INT NOT NULL
+user_id INTEGER,
+content TEXT NOT NULL,
+mark INTEGER DEFAULT 0,
+FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS messages(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-FOREIGN KEY (question_id) REFERENCES questions(id)
+question_id INTEGER,
+user_id INTEGER,
+content TEXT NOT NULL,
+mark INTEGER DEFAULT 0,
+FOREIGN KEY (question_id) REFERENCES questions(id),
 FOREIGN KEY (user_id) REFERENCES users(id)
-content TEXT NOT NULL
-mark INTEGER NOT NULL
-
+);
 CREATE TABLE IF NOT EXISTS votes(
-FOREIGN KEY (question_id) REFERENCES questions(id)
+question_id INTEGER,
+user_id INTEGER,
+FOREIGN KEY (question_id) REFERENCES questions(id),
 FOREIGN KEY (user_id) REFERENCES users(id)
 )
